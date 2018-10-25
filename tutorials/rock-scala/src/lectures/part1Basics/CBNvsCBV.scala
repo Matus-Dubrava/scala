14718166cd2869f781deff24c7e3fc0eb6b7d64e
matus dubrava <matusdubrava@gmail.com>
2018-10-25 12:05:25 +0200

    added call by name vs call by value tutorial code

--- tutorials/rock-scala/src/lectures/part1Basics/CBNvsCBV.scala
+++ tutorials/rock-scala/src/lectures/part1Basics/CBNvsCBV.scala
@@ -1,5 +1,22 @@
 package lectures.part1Basics
 
-object CBNvsCBV {
+object CBNvsCBV extends App {
+  def calledByValue(x: Long): Unit = {
+    println("by value: " + x)
+    println("by value: " + x)
+  }
 
+  def calledByName(x: => Long): Unit = {
+    println("by name: " + x)
+    println("by name: " + x)
+  }
+
+  calledByValue(System.nanoTime())
+  calledByName(System.nanoTime())
+
+  def infinite(): Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int) = print(x)
+
+//  printFirst(infinite(), 34) - This will crash
+  printFirst(34, infinite())
 }
